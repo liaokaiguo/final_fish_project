6b1ca07b8b9b9506f753e89506008037a83cdba9
Meijd0_0 <2567671112@qq.com>
2019-10-09 10:58:40 +0800

    外网后端映射地址修改

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 
 Vue.use(VueAxios,axios);
-axios.defaults.baseURL = "http://192.168.0.221:8080";
+axios.defaults.baseURL = "http://172.16.55.118:8080";// 外网映射后端地址
 // axios.defaults.baseURL = "http://192.168.43.40:8080";
 
 Vue.prototype.$echarts = echarts
