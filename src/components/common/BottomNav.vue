aa598aed9b13382b0db5516a843dfe570383355f
jackpan <pan860573@gmail.com>
2019-10-17 15:25:42 +0800

    实现底部导航栏目的组件化

--- src/components/common/BottomNav.vue
+++ src/components/common/BottomNav.vue
@@ -1,62 +1,41 @@
 <template>
-  <div class="BottomNav">
-    <ul>
-      <li>
-        <router-link to="#" class="lastbottomtext">地图显示</router-link>&nbsp;&nbsp;
-        <router-link to="#" class="lastbottomtext">渔船出入港</router-link>&nbsp;&nbsp;
-        <router-link to="#" class="lastbottomtext">渔船作业方式统计及查询</router-link>&nbsp;&nbsp;
-        <router-link to="#" class="lastbottomtext">船舶明细</router-link>&nbsp;&nbsp;
-        <router-link to="#" class="lastbottomtext">拖网作业方式统计及分析</router-link>&nbsp;&nbsp;
-        <router-link to="#" class="lastbottomtext">刺网作业方式统计及分析</router-link>&nbsp;&nbsp;
-        <router-link to="#" class="lastbottomtext">围网作业方式统计及分析</router-link>&nbsp;&nbsp;
-      </li>
-    </ul>
-  </div>
+	<div class="BottomNav">
+		<ul>
+			<li>
+				<router-link to="/mapShow" class="BottomNavText">地图显示</router-link>&nbsp;&nbsp;
+				<router-link to="/passPort" class="BottomNavText">渔船出入港</router-link>&nbsp;&nbsp;
+				<router-link to="/workModeSta" class="BottomNavText">渔船作业方式统计及查询</router-link>&nbsp;&nbsp;
+				<router-link to="#" class="BottomNavText">船舶明细</router-link>&nbsp;&nbsp;
+				<router-link to="/seineSA" class="BottomNavText">围网作业方式统计及分析</router-link>&nbsp;&nbsp;
+				<router-link to="/trawlSA" class="BottomNavText">拖网作业方式统计及分析</router-link>&nbsp;&nbsp;
+				<router-link to="/gillSA" class="BottomNavText">刺网作业方式统计及分析</router-link>&nbsp;&nbsp;
+				<router-link to="/stowSA" class="BottomNavText">张网作业方式统计及分析</router-link>&nbsp;&nbsp;
+			</li>
+		</ul>
+	</div>
 </template>
+
 <script>
+
 </script>
 
 <style scoped>
-h1,
-h2,
-h3,
-h4,
-h5,
-h6,
-ul,
-ol,
-li,
-a,
-input,
-button,
-select,
-span,
-td,
-table {
-  margin: 0;
-  font-weight: normal;
-  padding: 0;
-  list-style: none;
-}
-/*the last bottom image and text style*/
-.BottomNav {
-  align-content: center;
-  float: left;
-}
-
-.lastbottomtext {
-  align-content: center;
-  height: 20px;
-  font-family: MicrosoftYaHei;
-  font-size: 20px;
-  font-weight: normal;
-  font-stretch: normal;
-  letter-spacing: -1px;
-  color: #f7f7f7;
-  text-decoration: none;
-  margin-left: 10px;
-  padding: 0.5px;
-  float: left;
-}
+	.BottomNav {
+		align-content: center;
+		background-image: url("../../assets/lastbottomBg.png");
+		background-size: 100% 100%;
+		position: absolute;
+		top:94%;
+		left: 10%;
+		width: 80%;
+		height: 3%;
+		opacity: 0.66;
+	}
+	.BottomNavText {
+		height: 100%;
+		font-size: 1vw;
+		color: #f7f7f7;
+		text-decoration: none;
+	}
 </style>
     
