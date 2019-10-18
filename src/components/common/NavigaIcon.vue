d0e61c0cd8141e2d617ce91f94327942aab263da
jackpan <pan860573@gmail.com>
2019-10-18 08:56:15 +0800

    增加了右上角导航按钮的组件

---
+++ src/components/common/NavigaIcon.vue
@@ -0,0 +1,61 @@
+<template>
+	<div class="navigaIcon" id="navigaIconID">
+		<img class="goBack" src="../../assets/backico.png" alt="后退" v-on:click="goBack"
+		     v-bind:title="tipMsg.goBack">
+		<img class="goHome" src="../../assets/homeico.png" alt="回到主页" v-on:click="goHome"
+		     v-bind:title="tipMsg.goHome">
+	</div>
+</template>
+
+
+
+
+<script>
+	export default {
+		data() {
+			return {
+				// Tips提示
+				tipMsg:{
+					goBack: '后退',
+					goHome: '主页',
+				},
+			}
+		},
+		methods:{
+			goBack() {
+				this.$router.back(-1);
+			},
+			goHome() {
+				this.$router.push('/welcome')
+			},
+		}
+	}
+</script>
+
+<style scoped>
+	.navigaIcon {
+		position: absolute;
+		right: 2vw;
+		width: 15vw;
+		height: 5vh;
+		margin-top: 3.3vh;
+		margin-bottom: 0.1vh;
+		/* background-color 测试用 */
+		/* background-color: #FFFFFF; */
+	}
+
+	.navigaIcon .goBack {
+		cursor: pointer;
+		position: absolute;
+		right: 50%;
+		height: 90%;
+	}
+
+	.navigaIcon .goHome {
+		cursor: pointer;
+		position: absolute;
+		right: 28%;
+		height: 90%;
+	}
+</style>
+    
