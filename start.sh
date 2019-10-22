67cdd5f23892f3974af70601732b3564fcd6c6de
jx00109 <shu_ytj@163.com>
2019-10-22 16:13:51 +0800

    一键部署

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 npm run build
 
 APPNAME=myApp
-mkdir $APPNAME
+mkdir /home/ubuntu/tools/apache-tomcat-8.5.47/webapps/$APPNAME
 rm -rf /home/ubuntu/tools/apache-tomcat-8.5.47/webapps/$APPNAME/*
 scp -r scp -r dist/. /home/ubuntu/tools/apache-tomcat-8.5.47/webapps/$APPNAME/
 
