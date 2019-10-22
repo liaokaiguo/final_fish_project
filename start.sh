4f053723c0b7cefca34de6601cb9808f22c29d99
jx00109 <shu_ytj@163.com>
2019-10-22 16:17:39 +0800

    bug fixed

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+npm install chalk
 npm run build
 
 APPNAME=myApp
 mkdir /home/ubuntu/tools/apache-tomcat-8.5.47/webapps/$APPNAME
 rm -rf /home/ubuntu/tools/apache-tomcat-8.5.47/webapps/$APPNAME/*
-scp -r scp -r dist/. /home/ubuntu/tools/apache-tomcat-8.5.47/webapps/$APPNAME/
+scp -r dist/. /home/ubuntu/tools/apache-tomcat-8.5.47/webapps/$APPNAME/
 
 echo 'finished...'
