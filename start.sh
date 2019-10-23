53ea6a4d423c7c91a2da38f6e3d6d6dc4d91f67d
jx00109 <shu_ytj@163.com>
2019-10-23 09:10:13 +0800

    添加部署说明文档

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ rm -rf dist/
 
 npm run build
 
-APPNAME=myApp
+APPNAME=fbomi
 mkdir /home/ubuntu/tools/apache-tomcat-8.5.47/webapps/$APPNAME
 rm -rf /home/ubuntu/tools/apache-tomcat-8.5.47/webapps/$APPNAME/*
 scp -r dist/. /home/ubuntu/tools/apache-tomcat-8.5.47/webapps/$APPNAME/
